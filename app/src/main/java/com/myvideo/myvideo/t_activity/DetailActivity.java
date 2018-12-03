9c1d216baa9876a4f8cb15099bebe1dd141618dc
AppsLeloTsf <appslelo.com@gmail.com>
2018-12-03 15:05:19 +0530

    Bug Fixed

--- app/src/main/java/com/myvideo/myvideo/t_activity/DetailActivity.java
+++ app/src/main/java/com/myvideo/myvideo/t_activity/DetailActivity.java
@@ -3,6 +3,8 @@ package com.myvideo.myvideo.t_activity;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.TextView;
@@ -16,6 +18,8 @@ public class DetailActivity extends AppCompatActivity {
     private TextView textViewTitleDet;
     private TextView textViewDesDet;
     private ImageView imageViewDet;
+    private Button buttonPlay;
+    private Button buttonPause;
 
 
     @Override
@@ -27,6 +31,9 @@ public class DetailActivity extends AppCompatActivity {
         textViewDesDet = findViewById(R.id.tv_des_detail);
        // imageViewDet = findViewById(R.id.iv_det_img);
 
+        buttonPlay = findViewById(R.id.play);
+        buttonPause = findViewById(R.id.pause);
+
         String title = getIntent().getExtras().getString("VID_TITLE");
       //  String thumb = getIntent().getExtras().getString("VID_THUMB");
         String description = getIntent().getExtras().getString("VID_DES");
@@ -35,15 +42,28 @@ public class DetailActivity extends AppCompatActivity {
 
       //  String  = "type_here_the_link";
 
-        VideoView videoView = findViewById(R.id.video);
+        final VideoView videoView = findViewById(R.id.video);
         MediaController mc = new MediaController(this);
         mc.setAnchorView(videoView);
         mc.setMediaPlayer(videoView);
-        Uri video = Uri.parse(LINK);
+        final Uri video = Uri.parse(LINK);
         videoView.setMediaController(mc);
         videoView.setVideoURI(video);
         videoView.start();
 
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                videoView.start();
+            }
+        });
+
+        buttonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                videoView.stopPlayback();
+            }
+        });
         textViewTitleDet.setText(title);
         textViewDesDet.setText(description);
        // Glide.with(this).load(thumb).into(imageViewDet);
